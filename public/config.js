2414419360b970216b8d58d12fbd3cc6c54eb047
Mumbaibook <mumbaibookie@gmail.com>
2025-04-18 17:09:15 +0530

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
     API_URL: window.location.hostname === 'localhost' 
         ? 'http://localhost:3000' 
-        : 'https://your-project-name.vercel.app',
+        : 'https://mumbaibook.vercel.app/',
     SUPABASE_URL: 'https://eghyeniuubkgbumuhlbq.supabase.co',
     SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVnaHllbml1dWJrZ2J1bXVobGJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5NzM1NzMsImV4cCI6MjA2MDU0OTU3M30.qFfsZTh_r9vX0mQl-6Am2R5vo2HdCFPWjZVjU6wiwSg'  // Use the anon key here, NOT the service role key
 };
