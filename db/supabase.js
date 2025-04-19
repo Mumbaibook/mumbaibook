1c60f5d39ca6276bd438064ead57b89c3f28a5a3
Mumbaibook <mumbaibookie@gmail.com>
2025-04-19 16:45:51 +0530

    Update supabase.js

--- db/supabase.js
+++ db/supabase.js
@@ -1,18 +1,20 @@
 const { createClient } = require('@supabase/supabase-js');
 require('dotenv').config();
 
-const supabaseUrl = process.env.SUPABASE_URL || 'https://eghyeniuubkgbumuhlbq.supabase.co';
-const supabaseKey = process.env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVnaHllbml1dWJrZ2J1bXVobGJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5NzM1NzMsImV4cCI6MjA2MDU0OTU3M30.qFfsZTh_r9vX0mQl-6Am2R5vo2HdCFPWjZVjU6wiwSg';
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+    throw new Error('Missing Supabase URL or Key');
+}
 
 const supabase = createClient(supabaseUrl, supabaseKey, {
-  auth: {
-    autoRefreshToken: false,
-    persistSession: false
-  }
+    auth: {
+        autoRefreshToken: false,
+        persistSession: false
+    }
 });
 
 module.exports = supabase;
-
-
 
 
