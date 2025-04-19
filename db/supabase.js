87a6d1846ab9525107e4574255d3a772c605dba6
Mumbaibook <mumbaibookie@gmail.com>
2025-04-19 15:37:10 +0530

    Update supabase.js

--- db/supabase.js
+++ db/supabase.js
@@ -1,12 +1,11 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
-
-if (!supabaseUrl || !supabaseKey) {
-    throw new Error('Missing Supabase credentials');
-}
+// Use the same values as in public/config.js
+const supabaseUrl = 'https://eghyeniuubkgbumuhlbq.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVnaHllbml1dWJrZ2J1bXVobGJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5NzM1NzMsImV4cCI6MjA2MDU0OTU3M30.qFfsZTh_r9vX0mQl-6Am2R5vo2HdCFPWjZVjU6wiwSg';
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 module.exports = supabase;
+
+
