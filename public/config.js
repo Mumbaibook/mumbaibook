b0b5095b62f58ad6b1ea0e6c53557a6580c9fe7d
Mumbaibook <mumbaibookie@gmail.com>
2025-04-19 17:05:08 +0530

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
     API_URL: window.location.hostname === 'localhost' 
         ? 'http://localhost:3000' 
-        : '', // Empty string will make requests relative to current domain
+        : 'https://mumbaibook-git-main-mumbai-books-projects.vercel.app',
     SUPABASE_URL: 'https://eghyeniuubkgbumuhlbq.supabase.co',
     SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVnaHllbml1dWJrZ2J1bXVobGJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5NzM1NzMsImV4cCI6MjA2MDU0OTU3M30.qFfsZTh_r9vX0mQl-6Am2R5vo2HdCFPWjZVjU6wiwSg'
 };
